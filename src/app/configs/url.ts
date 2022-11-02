78504dfe137dedf4dd1f8a5649ff5bc051b27a01
fmikko <francismikko.antonio@lgusuite.com>
2022-11-02 10:24:27 +0800

    push

--- src/app/configs/url.ts
+++ src/app/configs/url.ts
@@ -1,2 +1,2 @@
-export const REMOTE_URL = 'https://gardner-server.herokuapp.com/';
+export const REMOTE_URL = 'https://gardner-server.herokuapp.com/api/v1';
 export const LOCAL_URL = 'http://localhost:3000/api/v1';
