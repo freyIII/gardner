ae04b1e792afabe401814a5ccbb2464a58498da4
fmikko <francismikko.antonio@lgusuite.com>
2022-11-16 12:21:33 +0800

    fix

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(forceSsl);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + "/dist/gardner-scheduling-system"));
+app.use(express.static(__dirname + "/dist/gardner-sceduling-system"));
 
 app.get("/*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "dist/gardner-scheduling-system/index.html")
+    path.join(__dirname, "dist/gardner-sceduling-system/index.html")
   );
 });
 
