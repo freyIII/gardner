4cb8a94dde35126bcd85ec1fb21d9ce533edac05
fmikko <francismikko.antonio@lgusuite.com>
2022-11-16 11:57:58 +0800

    dployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginModule } from './pages/login/login.module';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
