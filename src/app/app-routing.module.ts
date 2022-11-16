8649a642b78451946f827558f37f011cd487f26e
fmikko <francismikko.antonio@lgusuite.com>
2022-11-16 11:39:06 +0800

    route fixing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginModule } from './pages/login/login.module';
 
 const routes: Routes = [
-  { path: '/', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   {
-    path: 'login',
+    path: '/login',
     loadChildren: () =>
       import('./pages/login/login.module').then((m) => m.LoginModule),
   },
   {
-    path: 'portal',
+    path: '/portal',
     loadChildren: () =>
       import('./pages/portal/portal.module').then((m) => m.PortalModule),
   },
   {
-    path: 'reset-password',
+    path: '/reset-password',
     loadChildren: () =>
       import('./pages/reset-password/reset-password.module').then(
         (m) => m.ResetPasswordModule
