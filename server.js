6f264e7014bbf776ca43fcb0b6bfbe2f9fee4766
fmikko <francismikko.antonio@lgusuite.com>
2022-11-16 12:02:38 +0800

    dployment

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+var forceSsl = require("force-ssl-heroku");
+
+const app = express();
+
+app.use(forceSsl);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + "/dist/gardner-scheduling-system"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "dist/gardner-scheduling-system/index.html")
+  );
+});
+
+const port = process.env.PORT || "7777";
+app.set("port", port);
+
+/**
+ * Create HT TP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
